fc4b4a1bbfb2e3d6a97aaa7029d0342279a200a6
Bruno Martins <bgcngm@gmail.com>
2024-04-21 13:47:03 +0000

    Revert "Hookup SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC"
    This reverts commit bc8d7739ac4caee7b828477484e3ba4fbf36652b.
    
    Reason for revert: no longer needed when using blobs that match tag
    DISPLAY.LA.3.0.r1-10700-KAILUA.0.
    
    Change-Id: Ia89ce566a4cade6091a2dc6bc4e8b56ef7d1f73b
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -65,7 +65,6 @@ SOONG_CONFIG_leafQcomVars += \
     gralloc_handle_has_reserved_size \
     qti_vibrator_effect_lib \
     qti_vibrator_use_effect_stream \
-    sdmcore_has_is_display_hw_available_func \
     supports_extended_compress_format \
     uses_pre_uplink_features_netmgrd
 
@@ -80,7 +79,6 @@ SOONG_CONFIG_leafQcomVars_gralloc_handle_has_custom_content_md_reserved_size := 
 SOONG_CONFIG_leafQcomVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_leafQcomVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
 SOONG_CONFIG_leafQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
-SOONG_CONFIG_leafQcomVars_sdmcore_has_is_display_hw_available_func := $(TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC)
 SOONG_CONFIG_leafQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_leafQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
@@ -99,7 +97,6 @@ TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB ?= libperfmgr-ext
 TARGET_POWERSHARE_ENABLED ?= 1
 TARGET_POWERSHARE_DISABLED ?= 0
 TARGET_QTI_VIBRATOR_EFFECT_LIB ?= libqtivibratoreffect
-TARGET_SDMCORE_HAS_IS_DISPLAY_HW_AVAILABLE_FUNC ?= true
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 TARGET_TRUST_USB_CONTROL_PATH ?= /proc/sys/kernel/deny_new_usb
 TARGET_TRUST_USB_CONTROL_ENABLE ?= 1
